987678c9be57003b11c7c34834a41bee4a4e78c7
Jordan Ellis <jordanellis@Jordans-MacBook-Pro.local>
2018-12-02 23:45:37 -0500

    Added functionality to degrade a pencil tip with lowercase text.

--- src/main/java/pencil/durability/Pencil.java
+++ src/main/java/pencil/durability/Pencil.java
@@ -2,17 +2,23 @@ package pencil.durability;
 
 public class Pencil {
 	private final int pointDurabilityRating;
+	private int currentPointDurability;
 	private int eraserDurability;
 	private int pencilLength;
 
 	public Pencil(int initialPointDurability, int initialEraserDurability, int initialPencilLength){
 		this.pointDurabilityRating = initialPointDurability;
+		this.currentPointDurability = initialPointDurability;
 		this.eraserDurability = initialEraserDurability;
 		this.pencilLength = initialPencilLength;
 	}
 
 	public int getCurrentPointRemaining(){
-		return this.pointDurabilityRating;
+		return this.currentPointDurability;
+	}
+
+	public void setCurrentPointRemaing(int newPointRemaining){
+		this.currentPointDurability = newPointRemaining;
 	}
 
 	public int getCurrentEraserRemaining(){
@@ -21,5 +27,10 @@ public class Pencil {
 
 	public int length(){
 		return this.pencilLength;
+	}
+
+	public void write(String wordsToWrite, Paper paperToWriteOn){
+		setCurrentPointRemaing(getCurrentPointRemaining() - wordsToWrite.length());
+		paperToWriteOn.addText(wordsToWrite);
 	}
 }
--- src/test/java/pencil/durability/PencilTest.java
+++ src/test/java/pencil/durability/PencilTest.java
@@ -2,6 +2,7 @@ package pencil.durability;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 public class PencilTest {
 	@Test
@@ -13,5 +14,17 @@ public class PencilTest {
 		assertEquals(pointDurability, numberTwoPencil.getCurrentPointRemaining());
 		assertEquals(eraserDurability, numberTwoPencil.getCurrentEraserRemaining());
 		assertEquals(pencilLength, numberTwoPencil.length());
+	}
+
+	@Test
+	public void testThatUsingAPencilToWriteLowercaseTextOnAPaperDegradesThePointOfThePencil(){
+		int pointDurability = 10;
+		int eraserDurability = 50;
+		int pencilLength = 5;
+		Pencil numberTwoPencil = new Pencil(pointDurability, eraserDurability, pencilLength);
+		Paper mockPaper = mock(Paper.class);
+		numberTwoPencil.write("hello", mockPaper);
+		verify(mockPaper).addText("hello");
+		assertEquals(pointDurability - 5, numberTwoPencil.getCurrentPointRemaining());
 	}
 }
