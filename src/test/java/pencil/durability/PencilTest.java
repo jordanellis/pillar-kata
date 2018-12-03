c30668e3e6e7f8d470d86cb23a269aa056adac13
Jordan Ellis <jordanellis@Jordans-MacBook-Pro.local>
2018-12-02 23:55:53 -0500

    Refactored the PencilTest junit.

--- src/test/java/pencil/durability/PencilTest.java
+++ src/test/java/pencil/durability/PencilTest.java
@@ -9,23 +9,27 @@ public class PencilTest {
 	public static final int POINT_DURABILITY = 10;
 	public static final int ERASER_DURABILITY = 50;
 	public static final int PENCIL_LENGTH = 5;
+
+	Pencil numberTwoPencil;
+	Paper mockPaper;
+
 	@Before
 	public void setup(){
-		Pencil numberTwoPencil = new Pencil(POINT_DURABILITY, ERASER_DURABILITY, PENCIL_LENGTH);
-		Paper mockPaper = mock(Paper.class);
+		numberTwoPencil = new Pencil(POINT_DURABILITY, ERASER_DURABILITY, PENCIL_LENGTH);
+		mockPaper = mock(Paper.class);
 	}
 
 	@Test
 	public void testThatAPencilCanBeCreatedWithAPointDurabilityAnEraserDurabilityAndALength(){
-		assertEquals(pointDurability, numberTwoPencil.getCurrentPointRemaining());
-		assertEquals(eraserDurability, numberTwoPencil.getCurrentEraserRemaining());
-		assertEquals(pencilLength, numberTwoPencil.length());
+		assertEquals(POINT_DURABILITY, numberTwoPencil.getCurrentPointRemaining());
+		assertEquals(ERASER_DURABILITY, numberTwoPencil.getCurrentEraserRemaining());
+		assertEquals(PENCIL_LENGTH, numberTwoPencil.length());
 	}
 
 	@Test
 	public void testThatUsingAPencilToWriteLowercaseTextOnAPaperDegradesThePointOfThePencil(){
 		numberTwoPencil.write("hello", mockPaper);
 		verify(mockPaper).addText("hello");
-		assertEquals(pointDurability - 5, numberTwoPencil.getCurrentPointRemaining());
+		assertEquals(POINT_DURABILITY - 5, numberTwoPencil.getCurrentPointRemaining());
 	}
 }
